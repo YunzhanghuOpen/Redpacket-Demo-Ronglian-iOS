8f8a0943acb7c2fc5e8c1a676f8222f67f0c5cc4
都基鹏 <dylandu123@163.com>
2016-06-27 12:57:17 +0800

    删除荣誉代码

--- ECSDKDemo_OC/Redpacket/RedpacketSDK/RedpacketDemoViewController.m
+++ ECSDKDemo_OC/Redpacket/RedpacketSDK/RedpacketDemoViewController.m
@@ -136,11 +136,6 @@ static NSString *const RedpacketTakenMessageTipCellSenderIdentifier = @"Redpacke
 
     }
     
-//    struct objc_super sp;
-//    sp.receiver = self;
-//    sp.super_class = object_getClass(class_getSuperclass(self.class));
-//    double returnValue = ((double (*)(id self, SEL op, ...))objc_msgSend_fpret)(self, @selector(tableView:heightForRowAtIndexPath:),tableView,indexPath);
-    
     return [super tableView:tableView heightForRowAtIndexPath:indexPath];
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -177,99 +172,6 @@ static NSString *const RedpacketTakenMessageTipCellSenderIdentifier = @"Redpacke
 //    return ((id (*)(struct objc_super *super, SEL op, ...))objc_msgSendSuper)(&sp, @selector(tableView:cellForRowAtIndexPath:),tableView,indexPath);
     return [super tableView:tableView cellForRowAtIndexPath:indexPath];
 }
-//// 红包被抢消息处理
-//- (void)onRedpacketTakenMessage:(RedpacketMessageModel *)redpacket
-//{
-//    AVIMMessage *m = [RedpacketTakenAVIMMessage messageWithRedpacket:redpacket];
-//    [self.conversation sendMessage:m
-//                          callback:^(BOOL succeeded, NSError *error) {
-//                          }];
-//    RedpacketTakenAVIMTypedMessage *typedMessage = [RedpacketTakenAVIMTypedMessage messageWithAVIMMessage:m];
-//    SEL method = @selector(insertMessage:);
-//    [self callVoidSuperClass:[CDChatRoomVC class] method:method withObject:typedMessage];
-//}
-//
-//#pragma mark - 红包功能显示界面处理
-//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-//    id<XHMessageModel> message = [self.dataSource messageForRowAtIndexPath:indexPath];
-//    if ([message isKindOfClass:[RedpacketMessage class]]) {
-//        RedpacketMessage *redpacketMessage = (RedpacketMessage *)message;
-//        BOOL displayTimestamp = YES;
-//        BOOL displayPeerName = NO;
-//        if ([self.delegate respondsToSelector:@selector(shouldDisplayTimestampForRowAtIndexPath:)]) {
-//            displayTimestamp = [self.delegate shouldDisplayTimestampForRowAtIndexPath:indexPath];
-//        }
-//        if ([self.delegate respondsToSelector:@selector(shouldDisplayPeerName)]) {
-//            displayPeerName = [self.delegate shouldDisplayPeerName];
-//        }
-//        
-//        XHMessageTableViewCell *messageTableViewCell;
-//        switch (message.bubbleMessageType) {
-//            case XHBubbleMessageTypeReceiving:
-//                if (RedpacketMessageTypeRedpacket == redpacketMessage.redpacket.messageType) {
-//                    RedpacketMessageCell *redpacketCell = [tableView dequeueReusableCellWithIdentifier:RedpacketMessageCellReceiverIdentifier];
-//                    if (!redpacketCell) {
-//                        redpacketCell = [[RedpacketMessageCell alloc] initWithMessage:message
-//                                                                      reuseIdentifier:RedpacketMessageCellReceiverIdentifier];
-//                    }
-//                    redpacketCell.delegate = self;
-//                    redpacketCell.redpacketDelegate = self;
-//                    messageTableViewCell = redpacketCell;
-//                }
-//                else { // RedpacketMessageTypeTedpacketTakenMessage
-//                    RedpacketTakenMessageTipCell *redpacketCell = [tableView dequeueReusableCellWithIdentifier:RedpacketTakenMessageTipCellReceiverIdentifier];
-//                    if (!redpacketCell) {
-//                        redpacketCell = [[RedpacketTakenMessageTipCell alloc] initWithMessage:message
-//                                                                              reuseIdentifier:RedpacketTakenMessageTipCellReceiverIdentifier];
-//                    }
-//                    redpacketCell.delegate = self;
-//                    messageTableViewCell = redpacketCell;
-//                }
-//                break;
-//            case XHBubbleMessageTypeSending:
-//                displayPeerName = NO;
-//                if (RedpacketMessageTypeRedpacket == redpacketMessage.redpacket.messageType) {
-//                    RedpacketMessageCell *redpacketCell = [tableView dequeueReusableCellWithIdentifier:RedpacketMessageCellSenderIdentifier];
-//                    if (!redpacketCell) {
-//                        redpacketCell = [[RedpacketMessageCell alloc] initWithMessage:message
-//                                                                      reuseIdentifier:RedpacketMessageCellSenderIdentifier];
-//                    }
-//                    redpacketCell.delegate = self;
-//                    redpacketCell.redpacketDelegate = self;
-//                    messageTableViewCell = redpacketCell;
-//                }
-//                else {
-//                    RedpacketTakenMessageTipCell *redpacketCell = [tableView dequeueReusableCellWithIdentifier:RedpacketTakenMessageTipCellSenderIdentifier];
-//                    if (!redpacketCell) {
-//                        redpacketCell = [[RedpacketTakenMessageTipCell alloc] initWithMessage:message
-//                                                                              reuseIdentifier:RedpacketTakenMessageTipCellSenderIdentifier];
-//                    }
-//                    redpacketCell.delegate = self;
-//                    messageTableViewCell = redpacketCell;
-//                }
-//                break;
-//        }
-//        
-//        messageTableViewCell.indexPath = indexPath;
-//        if (RedpacketMessageTypeTedpacketTakenMessage == redpacketMessage.redpacket.messageType) {
-//            [messageTableViewCell configureCellWithMessage:message displaysTimestamp:NO displaysPeerName:NO];
-//        }
-//        else {
-//            [messageTableViewCell configureCellWithMessage:message displaysTimestamp:displayTimestamp displaysPeerName:displayPeerName];
-//        }
-//        [messageTableViewCell setBackgroundColor:tableView.backgroundColor];
-//        
-//        if ([self.delegate respondsToSelector:@selector(configureCell:atIndexPath:)]) {
-//            [self.delegate configureCell:messageTableViewCell atIndexPath:indexPath];
-//        }
-//        return messageTableViewCell;
-//    }
-//    else { // fallback to super
-//        return [super tableView:tableView cellForRowAtIndexPath:indexPath];
-//    }
-//}
-//
-
 - (void)redpacketCell:(RedpacketMessageCell *)cell didTap:(ECMessage *)message{
     if(RedpacketMessageTypeRedpacket == message.rpModel.messageType) {
         [self.redpacketControl redpacketCellTouchedWithMessageModel:message.rpModel];
